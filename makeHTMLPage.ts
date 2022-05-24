d41475742d319c61e091a8dd9ded961e20e730b9
Jonathan Hudak <hudak.jonathan@gmail.com>
2022-05-23 23:16:00 -0600

    Update makeHTMLPage.ts

--- makeHTMLPage.ts
+++ makeHTMLPage.ts
@@ -1,6 +1,4 @@
-import { serve } from "https://deno.land/std@0.137.0/http/server.ts";
-  
-interface HTMLPageOptions {
+ interface HTMLPageOptions {
   body?: string;
   head?: string;
   title: string;
@@ -12,6 +10,7 @@ export function makeHTMLPage({ body }: HTMLPageOptions) {
   <head>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>${title}</title>
+    ${head}
   </head>
   <body>
     ${body}
